6d12a80948417c3c80d95672c4c54b2f9b5f1467
Julen <wangxiujuni@gmail.com>
2022-02-06 18:22:03 +0800

    chore: update copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ export const Footer: React.FC<{
   return (
     <footer className={styles.footer}>
       <div className={styles.copyrightContainer}>
-        <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+        <div className={styles.copyright}>Copyright 2022 {config.author}</div>
         <div className={styles.copyright}>
           Powered by{' '}
           <a
